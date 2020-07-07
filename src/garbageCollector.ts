de05491409548bf39f8e2cb6438455d5538c244e
nthumann <me@n-thumann.de>
2020-07-07 10:56:07 +0200

    Adds Garbage Collector

---
+++ src/garbageCollector.ts
@@ -0,0 +1,28 @@
+import { firebaseConfig } from './environment';
+import * as firebase from 'firebase/app';
+import 'firebase/database'
+import { exit } from 'process';
+
+firebase.initializeApp(firebaseConfig);
+const realtimeDB = firebase.database().ref('assemblyPoints');
+const currentTime = Math.floor(Date.now() / 1000);
+const graceTime = 20;
+const promises: Promise<any>[] = [];
+
+realtimeDB.once('value', snapshot => {
+    snapshot.forEach(apSnapshot => {
+        apSnapshot.forEach(nextAPSnapshot => {
+            nextAPSnapshot.forEach(cyclist => {
+                const duration = cyclist.child('duration').val();
+                const timestamp = cyclist.child('timestamp').val();
+                if (timestamp + duration + graceTime < currentTime) {
+                    console.log(`${apSnapshot.key}/${nextAPSnapshot.key}/${cyclist.key} is ${Math.abs(timestamp + duration + graceTime - currentTime)} sec late! Removing...`);
+                    promises.push(cyclist.ref.remove());
+                }
+            });
+        });
+    });
+    Promise.all(promises).then(() => {
+        exit(0);
+    });
+});
